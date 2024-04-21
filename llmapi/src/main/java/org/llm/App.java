73be4ad704ce310e0af9d8bf2ffa2d1353dfe626
ashkrit@gmail.com <ashkrit@gmail.com>
2024-04-21 12:04:07 +0800

    Initial Project commit

---
+++ llmapi/src/main/java/org/llm/App.java
@@ -0,0 +1,13 @@
+package org.llm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
